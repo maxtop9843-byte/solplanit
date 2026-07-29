e97172e3209192b8e1a7216ea1576337d8bbec33
장형진 <maxtop9843@gmail.com>
2026-07-29 13:23:38 +0900

    fix(CALC-001): preserve sub-kilowatt capacity precision

--- src/lib/calculations/capacity.ts
+++ src/lib/calculations/capacity.ts
@@ -105,7 +105,7 @@ export function estimateInstallableCapacity(input: CapacityInput): CapacityResul
     panelFootprintM2: assumption.panelFootprintM2,
     panelCapacityKw: CAPACITY_METHOD.panelCapacityKw,
     panelCount,
-    installableCapacityKw: round(panelCount * CAPACITY_METHOD.panelCapacityKw, 1),
+    installableCapacityKw: round(panelCount * CAPACITY_METHOD.panelCapacityKw, 2),
     methodVersion: CAPACITY_METHOD.version,
   };
 }
